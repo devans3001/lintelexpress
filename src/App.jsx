1e2ef9588c22798674c52b49ee1a00aa5eced5bf
Evans Diegbe <devans30001@gmail.com>
2025-09-01 23:09:58 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import About from './pages/About';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
